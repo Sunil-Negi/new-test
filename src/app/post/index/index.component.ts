c8eaf07125b0e726b9b4639ddc176becb1cca28d
Sunil-Negi <nsunil213@gmail.com>
2024-01-26 19:37:49 +0530

    first commit

---
+++ src/app/post/index/index.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { Post } from '../post';
+import { PostService } from '../post.service';
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.css']
+})
+export class IndexComponent {
+  
+  posts: Post[] = [];
+    
+  /*------------------------------------------
+  --------------------------------------------
+  Created constructor
+  --------------------------------------------
+  --------------------------------------------*/
+  constructor(public postService: PostService) { }
+    
+  /**
+   * Write code on Method
+   *
+   * @return response()
+   */
+  ngOnInit(): void {
+    alert();
+
+  }
+    
+  /**
+   * Write code on Method
+   *
+   * @return response()
+   */
+  deletePost(id:number){
+    this.postService.delete(id).subscribe(res => {
+         this.posts = this.posts.filter(item => item.id !== id);
+         console.log('Post deleted successfully!');
+    })
+  }
+   
+}
